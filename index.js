03a94540097b7a6437dd72c66aaebf53052acd92
Matthias Giger <i@matthiasgiger.com>
2020-08-31 18:08:13 +0200

    fix(init): use CWD by default and go back when init configured

--- index.js
+++ index.js
@@ -6,47 +6,79 @@ import chalk from 'chalk'
 let color = 'gray'
 let name = 'log'
 
-try {
-  // __dirname is missing with ES modules.
-  // https://stackoverflow.com/a/62892482/3185545
-  const sourcePackagePath = join(
-    dirname(fileURLToPath(import.meta.url)),
-    '../..'
-  )
+const loadConfigurationFromPackage = (basePath) => {
+  try {
+    const packageJson = JSON.parse(readFileSync(join(basePath, 'package.json')))
 
-  const packageJson = JSON.parse(
-    readFileSync(join(sourcePackagePath, 'package.json'))
-  )
+    if (packageJson.name) {
+      name = packageJson.name
+    }
 
-  if (packageJson.name) {
-    name = packageJson.name
+    if (packageJson.color) {
+      color = packageJson.color
+    }
+  } catch (_) {
+    // Using defaults.
+  }
+}
+
+// If package imported regularly get config from CWD.
+loadConfigurationFromPackage(process.cwd())
+
+export const configure = (options) => {
+  if (typeof options !== 'object') {
+    console.log(
+      `${chalk.gray.bold('logua')} ${chalk.red.bold(
+        'Error'
+      )} No options object provided to configure(options).`
+    )
+    return
   }
 
-  if (packageJson.color) {
-    color = packageJson.color
+  if (options.init) {
+    try {
+      // __dirname is missing with ES modules.
+      // https://stackoverflow.com/a/62892482/3185545
+      const dirnameImport = join(
+        dirname(fileURLToPath(import.meta.url)),
+        // Need to go back to find the package when used with npm init.
+        '../..'
+      )
+
+      loadConfigurationFromPackage(dirnameImport)
+    } catch (_) {
+      // Using defaults.
+    }
   }
-} catch (_) {
-  // Using defaults.
+
+  if (options.name) {
+    name = options.name
+  }
+
+  if (options.color) {
+    color = options.color
+  }
 }
 
 export default (message, type) => {
   const namespace = chalk[color].bold(name)
 
+  // If no other punctuation provided all messages will end like a regular sentence.
+  const last = message.slice(-1)
+  const end = ['.', '!', '?', '\n'].includes(last) ? '' : '.'
+
   if (type === 'error') {
-    // eslint-disable-next-line no-console
-    console.log(`${namespace} ${chalk.red.bold('Error')} ${message}.\n`)
+    console.log(`${namespace} ${chalk.red.bold('Error')} ${message}${end}\n`)
     process.exit(0)
     return
   }
 
   if (type === 'warning') {
-    // eslint-disable-next-line no-console
     console.log(
-      `${namespace} ${chalk.rgb(255, 140, 0)('Warning')} ${message}.\n`
+      `${namespace} ${chalk.rgb(255, 140, 0)('Warning')} ${message}${end}\n`
     )
     return
   }
 
-  // eslint-disable-next-line no-console
-  console.log(`${namespace} ${message}.\n`)
+  console.log(`${namespace} ${message}${end}\n`)
 }
