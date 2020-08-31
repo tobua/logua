d8d40fb67e771dc9932b8a70c1bf7c206d8ecf0e
Matthias Giger <i@matthiasgiger.com>
2020-08-31 18:17:13 +0200

    fix(types): manual fix as typescript support still missing

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 // Reference: https://www.typescriptlang.org/play
 interface IOptions {
-  init?: boolean
-  name?: string
-  color?: string
+  init?: boolean;
+  name?: string;
+  color?: string;
 }
 export declare const configure: (options: IOptions) => void
 declare const _default: (message: string, type?: string) => void
